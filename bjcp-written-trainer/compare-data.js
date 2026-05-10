eaca6d255af556f10e6ed5286e403f48e2986050
KNW247 <doug@knightwalker.com>
2026-05-10 09:20:44 -0700

    Create compare-data.js

---
+++ bjcp-written-trainer/compare-data.js
@@ -0,0 +1,28 @@
+const compareProfiles = [
+    {
+        styleA: "5D",
+        styleB: "4A",
+
+        sharedIdentity: "clean German pale lagers",
+
+        keyDifference: "hop bitterness and dryness",
+
+        focus: [
+            {
+                category: "bitterness",
+                question: "Compared to Munich Helles, German Pils has:",
+                correct: "Higher"
+            },
+            {
+                category: "body",
+                question: "Compared to Munich Helles, German Pils is generally:",
+                correct: "Lighter"
+            },
+            {
+                category: "malt",
+                question: "Compared to Munich Helles, German Pils has:",
+                correct: "Less malt emphasis"
+            }
+        ]
+    }
+];
